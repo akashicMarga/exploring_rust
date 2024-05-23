cc21e2547d9b9f9f932f9f430099f58be60de7e3
Akash Singh <aki24695@gmail.com>
2024-05-23 15:33:59 +0530

    phi3 candle added

--- tauri_exps/src-tauri/src/main.rs
+++ tauri_exps/src-tauri/src/main.rs
@@ -1,10 +1,17 @@
+mod phi3;
+mod token_output_stream;
+mod utils;
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+
+    let mut  model = phi3::phi3::init();
+    let out = model.generate("you are helpful ai agent who is going to answer user queries be polite and precise.".to_string());
+    format!("Hello, {}! You've been greeted from Rust!, {:?}", name,out )
 }
 
 fn main() {
